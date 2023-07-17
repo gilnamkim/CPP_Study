8d2806ed16e4732c8bdacda0ddcbca1febfc9606
DustinKIM1 <21489716-DustinKIM1@users.noreply.replit.com>
2023-07-17 09:40:44 +0000

    bool타입

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
-#include <iostream>
+#include <stdio.h>
 
-int main() { std::cout << "Hello World!\n"; }
+int main() {
+  int age = 25;
+  bool isAdult = age > 19; // 대입식을 조건문으로 사용가능!
+  if (isAdult) {
+    puts("성인입니다.");
+  }
+}
