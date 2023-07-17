03397209e58c2bf5798613fb2c8ae334db16cd2d
gilnamkim <129935289+gilnamkim@users.noreply.github.com>
2023-07-17 11:16:52 +0000

    iostream

--- .ccls-cache/@home@runner@CPPStudy/chap01@newdelete.h
+++ .ccls-cache/@home@runner@CPPStudy/chap01@newdelete.h
@@ -17,6 +17,7 @@ int main() {
   printf("*pi = %d\n", *pi);
   delete pi; // 메모리를 해제할때 사용하는 연산자
 
+  
   // 배열의 메모리를 할당,해제하는 방법
   int *ar;
   ar = new int[5];
@@ -24,7 +25,7 @@ int main() {
     ar[i] = i;
   }
   for (int i = 0; i < 5; i++) {
-    printf("%d번째 = %d", i, ar[i]);
+    printf("%d번째 = %d\n", i, ar[i]);
   }
   delete[] ar;
 }
